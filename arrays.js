264584a60b3046e1708a20c3317867f8b55a8a81
James Stamps Jr <jstampshere@gmail.com>
2020-01-29 21:57:00 +0000

    Automatically backed up by Learn

---
+++ arrays.js
@@ -0,0 +1,34 @@
+var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
+
+function addElementToBeginningOfArray(array, element){
+    [element, ...array]
+      return [element, ...array]
+}
+function destructivelyAddElementToBeginningOfArray(array, element){
+    [array.unshift(element)]
+      return array
+}
+
+function addElementToEndOfArray(array, element){
+    [array.push(), element]
+      return [array.push(), element]
+}
+
+function destructivelyAddElementToEndOfArray(array, element){
+  [array.push(element)]
+    return array
+}
+
+function accessElementInArray(array, index){
+   return (array[2]);
+}
+
+function destructivelyRemoveElementFromBeginningOfArray(array){
+    [array.shift()];
+    return array
+}
+
+function removeElementFromBeginningOfArray(array){
+   [array.slice(1)];
+   return array.slice
+}
