76ba0c3063ff03a15d80080c6e54addb3bd01286
James Stamps Jr <jstampshere@gmail.com>
2020-01-29 22:01:45 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -32,3 +32,8 @@ function removeElementFromBeginningOfArray(array){
    [array.slice(1)];
    return array.slice(1)
 }
+
+function destructivelyRemoveElementFromEndOfArray(array){
+  [array.pop(1)];
+  return array
+}
